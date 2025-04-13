1ee072a6999ccad5e12197da8d0a0d80c02aa35a
DEMIxGODS <victorianovanenk@outlook.com>
2025-04-13 12:08:31 +0200

    Added working script.js file with JS test and toggleMode

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMode() {
+    document.body.classList.toggle("light-mode");
+  }
+  
+  console.log("JavaScript is working!");
+
+  console.log("✅ JavaScript is working, Victoriano!");
+alert("Hello, Victoriano! This message is from your script.js file 😄");
