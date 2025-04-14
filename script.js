6ade16b43ea2671f2e669dee6a552610b5a8b5fc
DEMIxGODS <victorianovanenk@outlook.com>
2025-04-14 06:25:56 +0200

    Updated all pages with hamburger menu, dark mode and style fixes

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function toggleMode() {
 
   console.log("✅ JavaScript is working, Victoriano!");
 alert("Hello, Victoriano! This message is from your script.js file 😄");
+// Check saved mode
+window.onload = function () {
+  const savedMode = localStorage.getItem("theme");
+  if (savedMode === "light") {
+    document.body.classList.add("light-mode");
+  }
+};
+
+function toggleMode() {
+  document.body.classList.toggle("light-mode");
+
+  // Save to localStorage
+  const mode = document.body.classList.contains("light-mode") ? "light" : "dark";
+  localStorage.setItem("theme", mode);
+}
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.classList.toggle("active");
+}
